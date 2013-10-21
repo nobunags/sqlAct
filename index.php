094b7966f0c2370996f9a2284d2c2c0de22f551b
nobunags <joseph_genio@outlook.com>
2013-10-21 21:37:13 +0800

    a
    a

--- index.php
+++ index.php
@@ -32,22 +32,6 @@ $pwd = "Joseph_genio1";
 $db = "nobunags";
 
 
-try{
-    $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
-    $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    $sql = "CREATE TABLE registration_tbl(
-    id INT NOT NULL IDENTITY(1,1) 
-    PRIMARY KEY(id),
-    name VARCHAR(30),
-    email VARCHAR(30),
-    date DATE)";
-    $conn->query($sql);
-}
-catch(Exception $e){
-    die(print_r($e));
-}
-echo "<h3>Table created.</h3>";
-
 // Connect to database.
 try {
     $conn = new PDO( "sqlsrv:Server= $host ; Database = $db ", $user, $pwd);
